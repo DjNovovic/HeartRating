98043bf76c2402d7dce92c85f45fccd6b0a427e2
DjNovovic <djordjijenovovic@gmail.com>
2021-09-10 20:18:11 +0200

    Heart Rating Finale

--- HeartRating/ContentView.swift
+++ HeartRating/ContentView.swift
@@ -8,14 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var rating: Int = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            VStack(spacing: 50) {
+                Text("Rating")
+                    .foregroundColor(Color(#colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)))
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                starsView
+                    .overlay(overlayView.mask(starsView))
+                Text("\(rating) Heart")
+                    .foregroundColor(Color(#colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)))
+                    .font(.title2)
+                    .fontWeight(.bold)
+            }
+        }
     }
+    
+    private var overlayView: some View {
+        GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                Rectangle()
+                    .fill(LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)), Color(#colorLiteral(red: 0.1294117719, green: 0.2156862766, blue: 0.06666667014, alpha: 1))]), startPoint: .leading, endPoint: .trailing))
+                    .frame(width: CGFloat(rating) / 5 * geometry.size.width)
+            }
+        }
+        .allowsHitTesting(false)
+    }
+    
+    private var starsView: some View {
+        HStack {
+            ForEach(1..<6) { index in
+                Image(systemName: "suit.heart.fill")
+                    .font(.largeTitle)
+                    .foregroundColor(Color.gray)
+                    .onTapGesture {
+                        withAnimation(.easeInOut) {
+                            rating = index
+                        }
+                    }
+            }
+        }
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            
     }
 }
