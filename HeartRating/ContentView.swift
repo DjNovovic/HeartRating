499710ccec51cb4c0d0714f197fc46a32b1b6a8d
DjNovovic <djordjijenovovic@gmail.com>
2021-09-10 20:11:23 +0200

    Initial Commit

---
+++ HeartRating/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HeartRating
+//
+//  Created by Đorđije Novović on 10.9.21..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HeartRating/HeartRatingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartRatingApp.swift
+//  HeartRating
+//
+//  Created by Đorđije Novović on 10.9.21..
+//
+
+import SwiftUI
+
+@main
+struct HeartRatingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
